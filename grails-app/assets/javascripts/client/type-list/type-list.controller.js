f54f2f9cabb7b182f1d7e1ab24cf056c81ddbeed
Matija Čvrk <matijacvrk@gmail.com>
2017-12-02 20:26:59 +0100

    (type-list-controller:angularjs) added angular controller for listing types

--- grails-app/assets/javascripts/client/type-list/type-list.controller.js
+++ grails-app/assets/javascripts/client/type-list/type-list.controller.js
@@ -5,17 +5,19 @@
         .module('grCommerce')
         .controller('TypeListController', TypeListController);
 
-    TypeListController.$inject = ['$scope', 'typeService'];
-    function TypeListController($scope, typeService) {
+    TypeListController.$inject = ['$scope', '$route', 'typeService', 'ROUTES'];
+    function TypeListController($scope, $route, typeService, ROUTES) {
         $scope.pageTitle = "List of product types";
         $scope.typeList = [];
+        $scope.linkToEdit = ROUTES['PRODUCT-TYPE-EDIT'].path;
+
+        $scope.removeType = removeType;
 
         init();
 
         function init() {
             typeService.getEntityList()
                 .then(function (response) {
-                    console.log(response.data);
                     var types = [];
                     angular.forEach(response.data, function (value, key) {
                         this.push(value);
@@ -23,6 +25,17 @@
                     $scope.typeList = types;
                 })
                 .catch(function (error) {
+                    console.log(error.message);
+                });
+        }
+
+        function removeType(id) {
+            typeService.deleteEntity(id)
+                .then(function (response) {
+                    $route.reload();
+                })
+                .catch(function (error) {
+                    alert(error.message);
                     console.log(error.message);
                 });
         }
