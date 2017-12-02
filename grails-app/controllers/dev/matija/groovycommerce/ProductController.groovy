97a683d17fc7c7b3f434b7d61040c158e5f2a8ce
Matija Čvrk <matijacvrk@gmail.com>
2017-12-02 20:28:21 +0100

    (product-controller:grails) added grails controller for products

--- grails-app/controllers/dev/matija/groovycommerce/ProductController.groovy
+++ grails-app/controllers/dev/matija/groovycommerce/ProductController.groovy
@@ -1,14 +1,44 @@
 package dev.matija.groovycommerce
 
+import grails.converters.JSON
+
 class ProductController {
+    ProductService productService
+    SortAndOrderService sortAndOrderService
 
-    def index() {}
+    def index() {
+        render(sortAndOrderService.sortAndOrderItemList(productService.getEntities(), "productName", "asc") as JSON)
+    }
 
-    def save() {}
+    def getList() {
+        render template: "list", contentType: "text/plain"
+    }
+
+    def getForm() {
+        render template: "form", contentType: "text/plain"
+    }
+
+    def getDetails() {
+        render template: "show", contentType: "text/plain"
+    }
+
+    def get(Long id) {
+        System.out.println("Product / get entity")
+        System.out.println(params)
+        render(productService.getEntity(id) as JSON)
+    }
+
+    def save() {
+        System.out.println("Product / save entity")
+        System.out.println(params)
+        render(productService.saveEntity(params) as JSON)
+    }
 
     def update() {}
 
-    def delete() {}
-
-    def show() {}
+    def delete(Long entityId) {
+        System.out.println("Product / delete entity")
+        System.out.println(entityId)
+        render(productService.removeEntity(entityId) as JSON)
+    }
 }
