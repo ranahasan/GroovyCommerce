07152f73d3d8e0a7e9306563cdb1fb853cb3da20
Matija Čvrk <matijacvrk@gmail.com>
2017-12-02 02:17:22 +0100

    (routes:angularjs) added a few new routes for products

--- grails-app/assets/javascripts/client/app.routes.js
+++ grails-app/assets/javascripts/client/app.routes.js
@@ -5,10 +5,10 @@
         .module('grCommerce')
         .constant('ROUTES', {
             'HOME': {path: '/home', title: 'Home'},
-            'PRODUCT-TYPE-LIST': {path: '/productType', title: 'Product types'},
-            'PRODUCT-TYPE-EDIT': {path: '/productType/edit', title: 'Edit product type'},
-            'PRODUCT-LIST': {path: '/product', title: 'Products'},
-            'PRODUCT-SHOW': {path: '/product/show', title: 'Show product'},
-            'PRODUCT-EDIT': {path: '/product/edit', title: 'Edit product'}
+            'PRODUCT-TYPE-LIST': {path: '/productType/', title: 'Product types'},
+            'PRODUCT-TYPE-EDIT': {path: '/productType/edit/', title: 'Edit product type'},
+            'PRODUCT-LIST': {path: '/product/', title: 'Products'},
+            'PRODUCT-SHOW': {path: '/product/show/', title: 'Show product'},
+            'PRODUCT-EDIT': {path: '/product/edit/', title: 'Edit product'}
         });
 })();
